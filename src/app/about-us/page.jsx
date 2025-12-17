ff9e29370b24f35a467537bda0d1104098c40355
manjimanm <manjimanm001@gmail.com>
2025-12-17 09:50:05 +0530

    about-s page mission vision section section started

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,14 @@
+import Header from '@/Components/Header'
+import React from 'react'
+import VisionMission from './MissionVision'
+
+function page() {
+  return (
+    <div>
+      <Header />
+      <VisionMission />
+    </div>
+  )
+}
+
+export default page
