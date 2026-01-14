a8c19de749ca48163d1b8dcfdd3d224269551b30
Ahammed Afnan <ahammedafnan.accolades@gmail.com>
2026-01-14 17:34:07 +0530

    created blog inner page

---
+++ src/app/blog/[slug]/BlogInner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import HeroSection from './HeroSection'
+import BlogContentSection from './BlogContentSection'
+
+export default function BlogInner() {
+    return (
+    <div>
+        <HeroSection/>
+        <BlogContentSection />
+    </div>
+    )
+}
