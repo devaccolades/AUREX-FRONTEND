ffba5c3261310d38b081de202bc340a0e57ef157
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2026-01-20 15:21:45 +0530

    updated

--- src/app/projects/[slug]/ProjectHeroSection.jsx
+++ src/app/projects/[slug]/ProjectHeroSection.jsx
@@ -10,62 +10,6 @@ import home from "../../../../public/images/projects/house 1.svg";
 import check from "../../../../public/images/projects/book-check 1.svg";
 import qr from "../../../../public/images/projects/REREA NUMBER LIARD 2.svg";
 
-// const project = {
-//   title: "Aurex Liard",
-//   location: "Mulangunnathukavu, Thrissur",
-//   rera: "K-RERA/PRJ/TSR/178/2023",
-//   qr_code: qr,
-//   short_title: "A Home That Reflects Your Aspirations",
-//   description:
-//     "Premium 2 & 3 BHK apartments at the heart of Thrissur with modern amenities and highway access.",
-//   image: "/images/projects/aurexliard.jpg",
-// };
-
-
-
-// const stats = [
-//   {
-//     label: "Property Type",
-//     value: "2 & 3 BHK Apartments",
-//     icon: building2,
-//   },
-//   {
-//     label: "Total Area",
-//     value: "23.4 Cents",
-//     icon: ruler,
-//   },
-//   {
-//     label: "Sqft Range",
-//     value: "951 – 1508 sqft",
-//     icon: home,
-//   },
-//   {
-//     label: "Total Units",
-//     value: "31 | Structure B+G+8",
-//     icon: check,
-//   },
-// ];
-
-// const highlights = [
-//   {
-//     title: "Prime Location",
-//     desc: "Mulangunnathukavu, Thrissur",
-//     icon: map,
-//   },
-//   {
-//     title: "Premium Apartments",
-//     desc: "2 & 3 BHK Luxury Living",
-//     icon: building,
-//   },
-//   {
-//     title: "Highway Facing",
-//     desc: "Excellent Connectivity",
-//     icon: spark,
-//   },
-// ];
-
-
-
 export default function ProjectHero({ project }) {
   if (!project) return null;
 
@@ -130,65 +74,61 @@ export default function ProjectHero({ project }) {
       desc: project.highlight_text_3,
       icon: spark,
     },
-  ].filter(h => h.title); // removes empty ones
+  ].filter((h) => h.title); // removes empty ones
 
+  console.log("check projects", project);
 
   return (
     <main>
-      <section className="relative w-full h-[80vh] md:h-[65vh] lg:h-[90vh] overflow-hidden">
+      <section className="relative w-full h-[80vh] md:h-[580px] lg:h-[780px] xl:h-[100vh] mb-10 overflow-hidden">
         {image && (
           <Image
             src={image}
             alt={image_alt || name}
             fill
-            className="object-cover object-center -z-10"
+            className="object-cover object-top -z-10"
           />
         )}
         <div className="absolute inset-0 bg-gradient-to-b from-black/0 to-black/60 md:hidden z-10" />
         <div className="absolute bottom-[10%] left-[5%] md:top-[30%] flex flex-col gap-y-2 md:gap-y-4 z-20">
-
           {status && (
             <button className="w-fit bg-[#006A54] text-white font-urban text-[16px] leading-[100%] font-extrabold  py-2 px-2.5 rounded-3xl">
               {status}
             </button>
           )}
-          <h1 className="w-[80%] lg:w-full text-wrap space-y-1 font-urban font-semibold text-white text-[32px] md:text-[36px] lg:text-[48px] leading-[100%]">
+          <h1 className="w-full lg:w-[70%] text-wrap space-y-1 font-urban font-semibold text-white text-[32px] md:text-[36px] lg:text-[48px] leading-[100%]">
             {project.name}
           </h1>
 
           {location && (
-            <div className="w-fit flex gap-2 py-2 px-2.5 bg-white/20 backdrop-blur-[9.7px] border border-gray-400 rounded-3xl">
+            <div className="w-fit flex gap-2 py-2 px-2.5 bg-white/10 backdrop-blur-[9.7px] border border-gray-400 rounded-3xl">
               <Image src={loc} alt="icon" />
-              <h3 className="text-white text-[13px] font-bold">{location}</h3>
+              <h3 className="text-white text-[13px] font-bold capitalize">
+                {location}
+              </h3>
             </div>
           )}
         </div>
 
-        <div className="absolute w-fit right-[5%] md:top-[30%] hidden md:grid grid-cols-1 gap-y-2 md:gap-y-4  ">
-          {/* <div className="flex gap-2 items-center ">
-            <Image src={project.qr_code} alt="icon" width={40} height={40} className="w-10 h-10" />
-            <div className="flex flex-col w-full">
-              <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-white text-[12px] leading-[100%]">
-                RERA REG. NO
-              </h1>
-              <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-white text-[12px] leading-[100%] whitespace-nowrap">
-                {project.k_rera}
-              </h1>
-            </div>
-          </div> */}
-          
+        <div className="absolute w-fit right-[5%] md:top-[30%] hidden md:grid grid-cols-1 gap-y-2 md:gap-y-4 ">
           {(qr_code || k_rera) && (
             <div className="flex gap-2 items-center">
               {qr_code && (
-                <Image src={qr_code} alt="RERA QR" width={40} height={40} className="w-10 h-10" />
+                <Image
+                  src={qr_code}
+                  alt="RERA QR"
+                  width={40}
+                  height={40}
+                  className="w-10 h-10"
+                />
               )}
 
               {k_rera && (
-                <div className="flex flex-col w-full">
-                  <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-white text-[12px] leading-[100%]">
+                <div className="flex flex-col w-full ">
+                  <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold  md:text-white text-[12px] leading-[100%]">
                     RERA REG. NO
                   </h1>
-                  <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-white text-[12px] leading-[100%] whitespace-nowrap">
+                  <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-black md:text-white text-[12px] leading-[100%] whitespace-nowrap">
                     {project.k_rera}
                   </h1>
                 </div>
@@ -196,7 +136,7 @@ export default function ProjectHero({ project }) {
             </div>
           )}
           <div className="max-w-[250px] flex flex-col">
-            <h3 className="font-urban font-medium text-[13px] leading-[156%] text-white">
+            <h3 className="font-urban font-medium text-[13px] leading-[156%] text-black md:text-white ">
               {project.sub_text}
             </h3>
             <p className="font-urban font-semibold text-[16px] leading-[156%] text-white">
@@ -211,7 +151,7 @@ export default function ProjectHero({ project }) {
           className="absolute bottom-0 left-0 w-full h-[50px] md:h-[120px] object-cover object-top z-10"
         />
 
-        <div className=" absolute bottom-0 left-[5%] lg:left-[10%] pb-10 z-20 lg:w-[80%] w-[90%] mx-auto hidden md:grid grid-cols-2 md:grid-cols-4 gap-3 lg:gap-5 ">
+        <div className=" absolute md:-bottom-8 left-[5%] lg:left-[10%] pb-10 z-20 lg:w-[80%] w-[90%] mx-auto hidden md:grid grid-cols-2 md:grid-cols-4 gap-3 lg:gap-5 ">
           {stats.map((item, index) => {
             return (
               <div
@@ -227,11 +167,11 @@ export default function ProjectHero({ project }) {
     `,
                 }}
               >
-                <div className="p-3 rounded-lg bg-[#E09B07]">
+                <div className="p-2.5 rounded-lg bg-[#E09B07]">
                   <Image
                     src={item.icon}
                     alt="icon"
-                    className="w-6 h-6 text-white"
+                    className="w-5 h-5 lg:w-6 lg:h-6 text-white"
                   />
                 </div>
 
@@ -251,22 +191,28 @@ export default function ProjectHero({ project }) {
 
       <div className=" w-[90%] mx-auto mb-5 md:hidden grid grid-cols-1 gap-y-2 md:gap-y-4">
         <div className="flex gap-2 items-center ">
-          <Image src={project.qr_code} alt="icon" width={40} height={40} className="w-10 h-10" />
+          <Image
+            src={project.qr_code}
+            alt="icon"
+            width={40}
+            height={40}
+            className="w-10 h-10"
+          />
           <div className="flex flex-col w-full">
             <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-black text-[12px] leading-[100%]">
               RERA REG. NO
             </h1>
             <h1 className="w-full text-wrap space-y-1 font-urban font-extrabold text-black text-[12px] leading-[100%] whitespace-nowrap">
-              {project.rera}
+              {project.k_rera}
             </h1>
           </div>
         </div>
         <div className="w-full flex flex-col">
           <h3 className="font-urban font-medium text-[13px] leading-[156%] text-black">
-            {project.short_title}
+            {project.sub_text}
           </h3>
-          <p className="font-urban font-semibold text-[16px] leading-[156%] text-black">
-            {project.description}
+          <p className="font-urban font-semibold text-[16px] leading-[120%] text-black">
+            {project.short_description}
           </p>
         </div>
       </div>
@@ -307,7 +253,7 @@ export default function ProjectHero({ project }) {
           );
         })}
       </div>
-      <div className="relative z-20 lg:w-[80%] w-[90%] mx-auto mt-3 grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div className="relative z-20 lg:w-[80%] w-[90%] mx-auto mt-5 grid grid-cols-1 md:grid-cols-3 gap-5 ">
         {highlights.map((item, index) => (
           <div
             key={index}
