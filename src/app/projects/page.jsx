61596813fdeae92671a57d4064cf531ec71c439f
adil-accolades <adil.accolades@gmail.com>
2026-01-22 11:35:40 +0530

    fixed seo and aminities button issue

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,29 +1,55 @@
-import Header from '@/components2/Header'
-import React from 'react'
-import HeroSection from './HeroSection'
-import Footer from '@/components2/Footer'
-import ProjectListing from './ProjectListing'
-import WhyChooseUs from '../(home)/WhyChoose'
-import HomeBuyingJourney from './HomeBuyingJourney'
-import BankingPartners from './BankingPartners'
-import SignatureProjects from './SignatureProjects'
+import Header from "@/components2/Header";
+import React from "react";
+import HeroSection from "./HeroSection";
+import Footer from "@/components2/Footer";
+import ProjectListing from "./ProjectListing";
+import WhyChooseUs from "../(home)/WhyChoose";
+import HomeBuyingJourney from "./HomeBuyingJourney";
+import BankingPartners from "./BankingPartners";
+import SignatureProjects from "./SignatureProjects";
 
-import { ProjectsFetch, SeoById } from '@/services/api';
+import { ProjectsFetch, SeoById } from "@/services/api";
 
-async function fetchSeoData(path) {
-  let data = {};
-  try {
-    const res = await SeoById(path);
-    console.log("Raw SEO API response:", res);
-    if (res?.data) {
-      data = res.data;
-    }
-  } catch (error) {
-    console.log(error);
+export async function generateMetadata() {
+  const seo = await SeoById("projects");
+
+  if (!seo) {
+    return {
+      title: "Aurex Builders",
+      description: "Aurex Builders",
+    };
   }
-  return data;
-}
 
+  return {
+    title: seo.meta_title,
+    description: seo.meta_description,
+
+    openGraph: {
+      title: seo.og_title || seo.meta_title,
+      description: seo.og_description || seo.meta_description,
+      url: "https://aurex.accoladesweb.com",
+      siteName: "Aurex Builders",
+      images: seo.og_image
+        ? [
+            {
+              url: seo.og_image, // FULL URL from backend
+              width: 1200,
+              height: 630,
+              alt: seo.og_title || seo.meta_title,
+            },
+          ]
+        : [],
+      type: "website",
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: seo.og_title || seo.meta_title,
+      description: seo.og_description || seo.meta_description,
+      images: seo.og_image ? [seo.og_image] : [],
+    },
+  };
+}
 
 export default async function page() {
   const projectsData = await ProjectsFetch();
@@ -32,14 +58,12 @@ export default async function page() {
     <div>
       <Header />
       <HeroSection />
-      <SignatureProjects data={projectsData}  />
-      <ProjectListing data={projectsData}/>
+      <SignatureProjects data={projectsData} />
+      <ProjectListing data={projectsData} />
       <HomeBuyingJourney />
       <WhyChooseUs />
       <BankingPartners />
       <Footer />
     </div>
-  )
+  );
 }
-
-
