e597332627d319497fd08609cac1dc6e7fa2f2c2
manjimanm <manjimanm001@gmail.com>
2026-01-22 11:29:36 +0530

    blog slug seo data

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -3,6 +3,45 @@ import BlogInner from './BlogInner'
 import Header from '@/components2/Header'
 import { BlogsById, BlogsFetch } from '@/services/api';
 
+
+/* 🔹 SEO META GENERATION */
+export async function generateMetadata({ params }) {
+  const slug = params.slug;
+  const blog = await BlogsById(slug);
+
+  if (!blog) {
+    return {
+      title: "Blog | Aurex Builders",
+      description: "Read latest updates and insights from Aurex Builders",
+    };
+  }
+
+  const title = blog.meta_title || blog.title;
+  const description =
+    blog.meta_description ||
+    blog.content?.replace(/<[^>]+>/g, "").slice(0, 160);
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      images: blog.image
+        ? [
+            {
+              url: blog.image, // must be absolute URL
+              width: 1200,
+              height: 630,
+            },
+          ]
+        : [],
+    },
+  };
+}
+
+
 export default async function page({ params }) {
   const slug = params.slug;
   const data = await BlogsById(slug); 
