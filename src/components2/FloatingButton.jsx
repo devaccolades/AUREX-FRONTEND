edce2032840fa37c80fe7e177b9c953b6730c794
Ahammed Afnan <ahammedafnan.accolades@gmail.com>
2026-02-02 12:37:11 +0530

    changes in home page herosection

--- src/components2/FloatingButton.jsx
+++ src/components2/FloatingButton.jsx
@@ -14,29 +14,48 @@ export default function FloatingButtons() {
         <>
 
             {/* <div className="absolute right-0 top-1/2 -translate-y-1/2 flex-col gap-1 hidden lg:flex"> */}
-             <div className="fixed -right-1 md:right-0 top-1/2 -translate-y-1/2 flex flex-col gap-0 md:gap-1  z-[999]">
+            <div
+                className="
+    fixed top-1/2 -translate-y-1/2 z-[999]
+    flex flex-col gap-1
+    right-0
+
+    transition-all duration-300 ease-in-out
+
+    lg:translate-x-[80%]
+    lg:hover:translate-x-0
+  "
+            >
+                {/* WhatsApp */}
                 <a
                     href="https://wa.me/919846507474?text=Hi%20I%20am%20interested%20in%20your%20project"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="bg-transparent"
                 >
                     <Image
                         src="/images/blog-inner/whatsapp.svg"
                         width={44}
                         height={44}
                         alt="Chat on WhatsApp"
-                        className="cursor-pointer w-[32px] h-[32px] md:w-[44px] md:h-[44px]"
+                        className="w-[36px] h-[36px] md:w-[40px] md:h-[40px] lg:w-[44px] lg:h-[44px]"
                     />
                 </a>
-                <button onClick={() => setOpenModal(true)}>
-                    <Image
-                        src="/images/blog-inner/message.svg"
-                        width={44}
-                        height={44}
-                        alt="chat-icon"
-                        className="cursor-pointer  w-[32px] h-[32px] md:w-[44px] md:h-[44px]" />
-                </button>
+
+                {/* Call */}
+                <a href="tel:+9846007474">
+                    <div className="bg-[#0a6e50] flex items-center justify-center py-[6px] lg:py-[8px] rounded-l-[15px]">
+                        <Image
+                            src="/images/telephone3.png"
+                            width={44}
+                            height={44}
+                            alt="call"
+                            className="w-[20px] h-[20px] lg:w-[22px] lg:h-[22px] invert brightness-100"
+                        />
+                    </div>
+                </a>
             </div>
+
 
             {openModal && (
                 <div className="fixed inset-0 z-[999] flex items-center justify-center bg-black/40 backdrop-blur-sm">
